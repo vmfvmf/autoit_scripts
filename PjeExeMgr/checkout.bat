bc49b33bdca995074fc1cf68daa2064f8f8a70f7
Vinicius Martins Ferraz <viniciusferraz@trt15.jus.br>
2021-05-05 18:24:14 -0300

    atualização

--- PjeExeMgr/checkout.bat
+++ PjeExeMgr/checkout.bat
@@ -8,49 +8,49 @@ if "%~1"=="" SET var="eb"
 if %var%=="eb" (
 title UPDATE EXE-BACKEND
 cd %EXEPJE_PATH%\exe-backend
-git pull
+git checkout %BNCH_EXE_B%
 )
 
 if "%~1"=="" SET var="ef"
 if %var%=="ef" (
 title UPDATE EXE-FRONTEND
 cd %EXEPJE_PATH%\exe-frontend
-git pull
+git checkout %BNCH_EXE_F%
 )
 
 if "%~1"=="" SET var="pa"
 if %var%=="pa" (
 title UPDATE PJE-ASSINATURA
 cd %EXEPJE_PATH%\pje-assinatura
-git pull
+git checkout %BNCH_PJE_A%
 )
 
 if "%~1"=="" SET var="pb"
 if %var%=="pb" (
 title UPDATE PJE-BACKEND
 cd %EXEPJE_PATH%\pje-backend
-git pull
+git checkout %BNCH_PJE_B%
 )
 
 if "%~1"=="" SET var="pcm"
 if %var%=="pcm" (
 title UPDATE PJE-CENTRAL MANDADOS
 cd %EXEPJE_PATH%\pje-centralmandados
-git pull
+git checkout %BNCH_PJE_CM%
 )
 
 if "%~1"=="" SET var="pcmf"
 if %var%=="pcmf" (
 title UPDATE PJE-CENTRAL MANDADOS FRONTEND
 cd %EXEPJE_PATH%\pje-centralmandados-frontend
-git pull
+git checkout %BNCH_PJE_CMF%
 )
 
 if "%~1"=="" SET var="pco"
 if %var%=="pco" (
 title UPDATE PJE-CORPORATIVO
 cd %EXEPJE_PATH%\pje-corporativo
-git pull
+git checkout %BNCH_PJE_CO%
 )
 
 
@@ -58,26 +58,26 @@ if "%~1"=="" SET var="pet"
 if %var%=="pet" (
 title UPDATE PJE-ETIQUETAS-BACKEND
 cd %EXEPJE_PATH%\pje-etiquetas-backend
-git pull
+git checkout %BNCH_PJE_PET_B%
 )
 
 if "%~1"=="" SET var="pi"
 if %var%=="pi" (
 title UPDATE PJE-INTEGRACAO
 cd %EXEPJE_PATH%\pje-integracao
-git pull
+git checkout %BNCH_PJE_I%
 )
 
 if %var%=="ps" (
 title UPDATE PJE-SEGURANCA
 cd %EXEPJE_PATH%\pje-seguranca
-git pull
+git checkout %BNCH_PJE_S%
 )
 
 if %var%=="psc" (
 title UPDATE PJE-SCRIPTS
 cd %EXEPJE_PATH%\pje-scripts
-git pull
+git checkout %BNCH_PJE_SC%
 )
 
 pause
